10f80d9c425f93513c115b1e08caa962aaa55d4d
Thiago1702 <duartethiago2010@gmail.com>
2019-07-05 19:03:50 -0300

    mudanças no java script
    correçao de bugs no javascript

--- Site confetaria/js/vform.js
+++ Site confetaria/js/vform.js
@@ -1,24 +1,21 @@
 function validar(){
 		
-		var nome = document.formcad.nome.value;
+		var nome = document.formcad.nome;
+		window.confirm(nome.value);
 		var email = document.getElementById('email');
 		var sexo = document.getElementById('sexo');
 		var senha = document.getElementById('senha');
 		var senha2 = document.getElementById('senha2');
-		var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+		var filtro = "/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i";
 		
-		var contErro = ;
+		var contErro = 0;
 		
-		if(nome == ""){
-		
-		
+		if(nome.value == ""){
+			
 			document.getElementById('alertanome').style.display = "block";
-		
 			document.getElementById('labnome').classList.add("text-danger");
 		
 			nome.classList.add("is-invalid");
-		
-		
 			nome.focus();
 			return false;
 		
